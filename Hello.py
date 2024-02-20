2541ed20598d21e65e20a429ceadb235de07f148
phatquantcorner <159128452+phatquantcorner@users.noreply.github.com>
2024-02-20 15:16:05 +0000

    asd

--- Hello.py
+++ Hello.py
@@ -17,34 +17,41 @@ from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import warnings
+
+warnings.filterwarnings("ignore")
+pd.options.display.float_format = '{:.4%}'.format
+
+# Date range
+start = '2016-01-01'
+end = '2019-12-30'
+
+# Tickers of assets
+assets = ['JCI', 'TGT', 'CMCSA', 'CPB', 'MO', 'APA', 'MMC', 'JPM',
+          'ZION', 'PSA', 'BAX', 'BMY', 'LUV', 'PCAR', 'TXT', 'TMO',
+          'DE', 'MSFT', 'HPQ', 'SEE', 'VZ', 'CNP', 'NI', 'T', 'BA']
+assets.sort()
+
+# Downloading data
+data = yf.download(assets, start = start, end = end)
+data = data.loc[:,('Adj Close', slice(None))]
+data.columns = assets
+
+
+
+
 
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="Portopt",
+        page_icon="🖥",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write(data)
 
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
 
 
 if __name__ == "__main__":
